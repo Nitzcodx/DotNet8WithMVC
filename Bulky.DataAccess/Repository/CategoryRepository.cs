c7cc88a9c69ce050f2d4a9f5a41f6599a357e1f7
Nitish Saxena <nitish_saxena@hotmail.com>
2023-07-12 14:12:44 +0530

    Repository Pattern Part 1

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly BulkyDbContext _bulkyDbContext;
+
+        public CategoryRepository(BulkyDbContext bulkyDbContext) : base(bulkyDbContext)
+        {
+            _bulkyDbContext = bulkyDbContext;
+        }
+
+        public void Save()
+        {
+            _bulkyDbContext.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            _bulkyDbContext.Categories.Update(category);
+        }
+    }
+}
---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly BulkyDbContext _bulkyDbContext;
+
+        internal DbSet<T> _dbSet;
+
+        public Repository(BulkyDbContext bulkyDbContext)
+        {
+            _bulkyDbContext = bulkyDbContext;
+            _dbSet = _bulkyDbContext.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public T Get(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = _dbSet;
+
+            query = query.Where(filter);
+
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}
